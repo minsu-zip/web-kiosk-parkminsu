5af4275698518799765e19b7c8b6976ae8ee8137
minsu-zip <alstn4814@gmail.com>
2022-08-04 14:50:13 +0900

    feat : 환경변수 파일 정의 및 TypeOrm 정의

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { ConfigModule } from '@nestjs/config'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { MySqlConfigModule } from './config/database/config.module'
+import { MySqlConfigService } from './config/database/config.service'
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [MySqlConfigModule],
+      useClass: MySqlConfigService,
+      inject: [MySqlConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

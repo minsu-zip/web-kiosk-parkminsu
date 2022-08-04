185bfa0964b1a3e0a4fa1a6287d9797216f0e318
minsu-zip <alstn4814@gmail.com>
2022-08-04 14:49:38 +0900

    feat : db 연결 service, module 정의

---
+++ packages/server/src/config/database/config.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm'
+
+@Injectable()
+export class MySqlConfigService implements TypeOrmOptionsFactory {
+  constructor(private configService: ConfigService) {}
+
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    return {
+      type: 'mysql',
+      host: this.configService.get<string>('MYSQL_HOST'),
+      username: this.configService.get<string>('MYSQL_USER'),
+      password: this.configService.get<string>('MYSQL_PW'),
+      port: +this.configService.get<number>('NYSQL_PORT'),
+      database: this.configService.get<string>('MYSQL_DB'),
+      entities: ['dist/**/**/*.entity{.ts,.js}'],
+      synchronize: true,
+      // autoLoadEntities: true,
+    }
+  }
+}

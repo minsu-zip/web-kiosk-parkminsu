6150cd58c187739660abf2d52bf2a7288852823b
minsu-zip <alstn4814@gmail.com>
2022-08-05 22:10:46 +0900

    chore : 목업데이터 변경

--- packages/server/src/app.service.ts
+++ packages/server/src/app.service.ts
@@ -106,6 +106,96 @@ export class AppService {
         {
           id: 3,
           name: '비타민',
+          menus: [
+            {
+              id: 1,
+              name: 'jlkjkljkljl',
+              imgUrl1:
+                'https://static.thcdn.com/images/xsmall/webp//productimg/original/10530943-1224889444460882.jpg',
+              imgUrl2:
+                'https://static.thcdn.com/images/xsmall/webp//productimg/original/10530943-4134889444511789.jpg',
+              price: 20000,
+              sellCount: 4,
+              options: [
+                {
+                  id: 1,
+                  name: '맛',
+                  detail: [
+                    {
+                      id: 1,
+                      name: '초코 맛',
+                      price: 0,
+                    },
+                    {
+                      id: 2,
+                      name: '초코브라우니 맛',
+                      price: 1000,
+                    },
+                  ],
+                },
+                {
+                  id: 2,
+                  name: '사이즈',
+                  detail: [
+                    {
+                      id: 1,
+                      name: '5 kg',
+                      price: 0,
+                    },
+                    {
+                      id: 2,
+                      name: '10 kg',
+                      price: 15000,
+                    },
+                  ],
+                },
+              ],
+            },
+            {
+              id: 2,
+              name: 'kljkljkljklllkj',
+              imgUrl1:
+                'https://static.thcdn.com/images/xsmall/webp//productimg/1600/1600/12457913-3554790136840897.jpg',
+              imgUrl2:
+                'https://static.thcdn.com/images/xsmall/webp//productimg/original/12081395-1194954986226116.jpg',
+              price: 35000,
+              sellCount: 2,
+              options: [
+                {
+                  id: 1,
+                  name: '맛',
+                  detail: [
+                    {
+                      id: 1,
+                      name: '초코 맛',
+                      price: 0,
+                    },
+                    {
+                      id: 2,
+                      name: '초코브라우니 맛',
+                      price: 1000,
+                    },
+                  ],
+                },
+                {
+                  id: 2,
+                  name: '사이즈',
+                  detail: [
+                    {
+                      id: 1,
+                      name: '5 kg',
+                      price: 0,
+                    },
+                    {
+                      id: 2,
+                      name: '10 kg',
+                      price: 15000,
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
         },
         {
           id: 4,

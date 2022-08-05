4d7fea2a8907da5f2d0a5ca0fd5e1bfb72fc2597
minsu-zip <alstn4814@gmail.com>
2022-08-05 22:20:11 +0900

    refactor : tab, tabItem 구조 변경

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -5,14 +5,23 @@ import GlobalStyle from './styles/GlobalStyles'
 import { getAllInfo } from './apis/kiosk'
 import Tab from 'atoms/Tab'
 import styled from '@emotion/styled'
+import TabItem from 'atoms/Tab/TabItem'
 
 const App = () => {
+  // 데이터 필드 변경에 따른 ts타입 나중에 변경 예정
   const [kioskData, setKioskData] = useState<any>()
+  const [selected, setSelected] = useState<number>()
+  const [menuList, setMenuList] = useState<any>()
 
   const getData = useCallback(async () => {
     const { categories } = await getAllInfo()
     setKioskData(categories)
-  }, [])
+  }, [kioskData])
+
+  const onClickCategory = (id: number) => {
+    setSelected(id)
+    setMenuList(kioskData[id - 1])
+  }
 
   useEffect(() => {
     getData()
@@ -21,16 +30,36 @@ const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
+      <div className="App">
+        <Wrapper>
+          {kioskData ? (
+            kioskData.map(({ id, name }: any) => (
+              <Tab
+                key={`${id}${name}`}
+                id={id}
+                category={name}
+                active={selected === id}
+                onClickCategory={onClickCategory}
+              />
+            ))
+          ) : (
+            <div>로딩중</div>
+          )}
+        </Wrapper>
 
-      {kioskData ? (
-        <div className="App">
-          <Tab kioskData={kioskData}></Tab>
-        </div>
-      ) : (
-        <div>로딩중</div>
-      )}
+        {menuList?.menus?.map(({ id, name }: any) => (
+          <TabItem key={id} id={id} title={name}></TabItem>
+        ))}
+      </div>
     </ThemeProvider>
   )
 }
+
+const Wrapper = styled.div`
+  margin-top: 30px;
+  display: flex;
+  justify-items: center;
+  justify-content: space-around;
+`
 
 export default App

2f4c9aa4d1e80463483ef0087478c38526d67175
minsu-zip <alstn4814@gmail.com>
2022-08-08 14:25:31 +0900

    feat : 옵션정보 UI

--- packages/client/src/components/MenuOption/index.tsx
+++ packages/client/src/components/MenuOption/index.tsx
@@ -1,8 +1,6 @@
 import { TMenuOption } from 'types'
 import React from 'react'
-import OptionDetail from 'components/OptionDetail'
 import styled from '@emotion/styled'
-import checked from '../../images/checked.svg'
 import unchecked from '../../images/unchecked.svg'
 
 interface Props {
@@ -12,33 +10,42 @@ interface Props {
 const MenuOption: React.FC<Props> = ({ options }) => {
   return (
     <>
-      {options.map(({ detail, name }) => (
-        <>
-          {name}
-          {detail.map(({ id, name, price }) => (
-            <OptionWrapper data-id={id}>
-              <div>
-                <img src={unchecked} />
-                <img src={checked} />
-              </div>
-              <Span>{name}</Span>
-              <Span>{price.toLocaleString()}</Span>
-            </OptionWrapper>
-          ))}
-        </>
-      ))}
+      <OptionContainer>
+        {options.map(({ detail, name }) => (
+          <>
+            <Div>{name}</Div>
+            {detail.map(({ id, name, price }) => (
+              <OptionWrapper data-id={id}>
+                <Div>
+                  <img src={unchecked} />
+                </Div>
+
+                <Div>{name}</Div>
+                <Div>{price.toLocaleString()}원</Div>
+              </OptionWrapper>
+            ))}
+          </>
+        ))}
+      </OptionContainer>
     </>
   )
 }
+const OptionContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-items: center;
+  margin-left: 30px;
+  margin-top: 30px;
+`
 
 const OptionWrapper = styled.div`
-  display: flex;
-  height: 40px;
+  flex-direction: column;
+  height: 50px;
   align-items: center;
+  margin: 10px;
 `
 
-const Span = styled.span`
-  margin: 5px;
+const Div = styled.span`
+  margin-left: 10px;
 `
-
 export default MenuOption

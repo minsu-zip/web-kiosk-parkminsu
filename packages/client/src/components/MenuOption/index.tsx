5a0db42f93825ce395e4d7f66815402b0f185a00
minsu-zip <alstn4814@gmail.com>
2022-08-09 14:17:14 +0900

    refactor : 메뉴 옵션 디테일 로직 분리

--- packages/client/src/components/MenuOption/index.tsx
+++ packages/client/src/components/MenuOption/index.tsx
@@ -1,5 +1,5 @@
 import { TMenuOption } from 'types'
-import React, { useCallback, useState } from 'react'
+import React from 'react'
 import styled from '@emotion/styled'
 import OptionDetail from 'components/OptionDetail'
 
@@ -8,37 +8,15 @@ interface Props {
 }
 
 const MenuOption: React.FC<Props> = ({ options }) => {
-  const [selectedOption, setSelectedOption] = useState(
-    options.map(({ id }) => ({ optionId: id, detailId: 0 })),
-  )
-
-  const onClickSelectedOption = useCallback(
-    (optionIndex: number, detailId: number) => {
-      const newSelectedOption = [...selectedOption]
-      newSelectedOption[optionIndex].detailId = detailId
-      setSelectedOption(newSelectedOption)
-    },
-    [selectedOption],
-  )
-
   return (
-    <>
-      <OptionContainer>
-        {options.map(({ detail, name, id }, idx) => (
-          <>
-            <Div>{name}</Div>
-            {detail.map((option, index) => (
-              <OptionDetail
-                option={option}
-                index={index}
-                selectedOption={selectedOption[idx]}
-                onClickSelectedOption={(id) => onClickSelectedOption(idx, id)}
-              />
-            ))}
-          </>
-        ))}
-      </OptionContainer>
-    </>
+    <OptionContainer>
+      {options.map(({ name, detail }) => (
+        <>
+          <Div>{name}</Div>
+          <OptionDetail detail={detail}></OptionDetail>
+        </>
+      ))}
+    </OptionContainer>
   )
 }
 

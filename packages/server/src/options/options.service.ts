f24fdc2ba944077010a42c6fb9403b88508e3971
minsu-zip <alstn4814@gmail.com>
2022-08-10 13:03:43 +0900

    feat : 옵션에 대한 디테일  정보 가져오기

--- packages/server/src/options/options.service.ts
+++ packages/server/src/options/options.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common'
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
 import { Repository } from 'typeorm'
 import { Option } from './entities/option.entity'
@@ -9,8 +9,20 @@ export class OptionsService {
     @InjectRepository(Option) private optionRepository: Repository<Option>,
   ) {}
 
-  findAll(): Promise<Option[]> {
-    return this.optionRepository.find()
+  async findAll() {
+    try {
+      const data = await this.optionRepository
+        .createQueryBuilder('option')
+        .leftJoinAndSelect('option.details', 'details')
+        .getMany()
+
+      if (!data)
+        throw new HttpException('옵션 정보가 없습니다.', HttpStatus.NOT_FOUND)
+
+      return data
+    } catch (error) {
+      throw new HttpException(error, HttpStatus.BAD_GATEWAY)
+    }
   }
 
   findOne(id: number): Promise<Option> {

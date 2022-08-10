5b8dc4ae0ca977c2b98d19ae4a78d2a85ec9c574
minsu-zip <alstn4814@gmail.com>
2022-08-10 17:50:42 +0900

    chore : name필드 unique추가

--- packages/server/src/options/entities/option.entity.ts
+++ packages/server/src/options/entities/option.entity.ts
@@ -1,8 +1,9 @@
-import { Column, Entity, OneToMany } from 'typeorm'
+import { Column, Entity, OneToMany, Unique } from 'typeorm'
 import { OptionDetail } from './optionDetail.entity'
 import { CommonEntity } from '@common/CommonEntity'
 
 @Entity({ name: 'option_table' })
+@Unique(['name'])
 export class Option extends CommonEntity {
   @Column({ type: 'varchar', length: 30 })
   name!: string

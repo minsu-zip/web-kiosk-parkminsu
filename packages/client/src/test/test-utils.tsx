fdd4c5add2feeebab7e92d1c4f2458c93753a734
minsu-zip <alstn4814@gmail.com>
2022-08-11 15:14:26 +0900

    feat : 테스트 코드 공통 모듈 정의

---
+++ packages/client/src/test/test-utils.tsx
@@ -0,0 +1,20 @@
+import { ThemeProvider } from '@emotion/react'
+import { render, RenderOptions } from '@testing-library/react'
+import React, { ReactElement } from 'react'
+import { theme } from 'utils/styles'
+
+interface Props {
+  children: React.ReactNode
+}
+
+export const CustomProvider = ({ children }: Props) => {
+  return <ThemeProvider theme={theme}>{children}</ThemeProvider>
+}
+
+const customRender = (
+  ui: ReactElement,
+  options?: Omit<RenderOptions, 'wrapper'>,
+) => render(ui, { wrapper: CustomProvider, ...options })
+
+export * from '@testing-library/react'
+export { customRender as render }
